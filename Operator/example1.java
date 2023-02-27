a7135ca18a77546b490a168421817688185e32fa
Abhishek Khinde <112416277+Abhishekk0408@users.noreply.github.com>
2023-02-27 22:49:14 +0530

    Add files via upload

--- Operator/example1.java
+++ Operator/example1.java
@@ -1,28 +1,14 @@
-/*Assign values of variables 'a' and 'b' as 55 and 70 respectively and then check if:
-     i) both the conditions 'a < 50' and 'a < b' are true.
-    ii) at least one of the conditions 'a < 50' or 'a < b' is true.
-*/
-
 import java.util.Scanner;
 
+//- Write a program to add 8 to the number x and then divide it by 3. Now, the modulus of the quotient
+//is taken with 5 and then multiply the resultant value by 5. Display the final result.
+
+import java.util.Scanner;
 public class example1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter a");
-        int a = sc.nextInt();
-        System.out.println("enter b");
-        int b = sc.nextInt();
-
-        System.out.println(a);
-        System.out.println(b);
-
-        // both the conditions 'a < 50' and 'a < b' are true.
-        System.out.println("a < 50 and a < b");
-        System.out.println(a < 50 && a < b);
-
-        // at least one of the conditions 'a < 50' or 'a < b' is true.\
-        System.out.println("a < 50 or a < b");
-        System.out.println(a < 50 || a < b);
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        System.out.print((((x + 8) / 3) % 5) * 5);
     }
-
 }
+
